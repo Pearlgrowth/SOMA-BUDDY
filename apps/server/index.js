09737883a9817a63af67b94e7dc74e0ecdbc7c40
jack-khalif <mugwewaithaka2@gmail.com>
2025-08-30 15:07:18 +0300

    Integrate shared utils and enhance SomaBuddy branding

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,13 +1,13 @@
 import express from "express";
-//import { greet } from "@shared/utils";
+import { greet } from "@repo/utils";
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-res.send("This is greeting from common code");
+  res.send(greet("This is greeting from common code"));
 });
 
 app.listen(port, () => {
-console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
